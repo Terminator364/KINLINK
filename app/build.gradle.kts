a475d40d74a3a2cea53812e8945dae8ea4971086
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-20 16:38:58 +0100

    fix: avoid duplicate Kotlin plugin application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -26,7 +25,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
