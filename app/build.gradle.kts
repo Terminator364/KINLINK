4d7720d1aa2109096c2bff3feb28b0fca5e127e2
Terminator364 <158576221+Terminator364@users.noreply.github.com>
2026-09-20 19:30:48 +0100

    [skip ci] chore: prepare integrated 0.4.0-rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.terminator364.kinlink"
         minSdk = 30
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.3.0-m3"
+        versionCode = 5
+        versionName = "0.4.0-rc1"
     }
 
     signingConfigs {
